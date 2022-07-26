9c25d47ad50ae6f422b3557d89607952e642b9f2
zhangjei1 <109933102+zhangjei1@users.noreply.github.com>
2022-07-26 17:52:09 +0800

    11

--- src/components/three-scene/Change.js
+++ src/components/three-scene/Change.js
@@ -1,76 +1,91 @@
-/**
- * 交付项目时场景不可留有log *** 
- * 记得开启
- */
 // const console = {
 //   log: () => { }
 // }
 
-import { Utils } from "run-scene-v2";
-import bus from "./Bus";
 // 声明变量
+import bus from './Bus'
+import { Utils } from "run-scene-v2";
+import * as THREE from "three";
 let camera, scene, controls, renderer2, renderer, dom, t, p, runScene, Bus;
-// 工具
-const { getRes, getMacro } = Utils;
+Bus = bus
 // 拿资源
 const setAssets = (assets) => {
   camera = assets.camera;
   scene = assets.scene;
   controls = assets.controls;
+  // renderer2 = assets.renderer2;
   renderer = assets.renderer;
   dom = assets.engineDom;
   t = assets.t;
-  // renderer2 = assets.renderer2;
   // p = assets.p;
 };
-
-//  页面接口总出口
-function Change(runScene) {
-  /* 拿资源 分解资源
-        this挂载至t上
-        runScene上的其他Api可以直接runScene.直接使用
-    */
+// 整体场景事件
+function Change(runScene, onDone) {
+  /* 拿资源 分解资源 
+      this挂载至t上 
+      runScene上的其他Api可以直接runScene.直接使用
+  */
   setAssets({ ...runScene.assetsEx.get(), t: this, runScene });
-
+  this.getModel = (name) => {
+    const id = runScene.uniqueEx.model.nameMap[name];
+    if (!id) return;
+    return runScene.uniqueEx.model.get(id);
+  };
   // 挂载runScene
   t.runScene = runScene;
 
-  // 懒加载结束回调
+  console.log(runScene, "runScene");
+  console.log(Utils, "Utils");
+
+
+  //入场动画
+  this.entryAnima = () => {
+    runScene.modelEx.camAnima(
+      runScene.modelEx.getCamLocal(),
+      {
+        cx:101.51047171910608,cy:1507.888677626636,cz:1584.0585568415909,tx:306.3070021011309,ty:296.623976,tz:-196.23492809703617 
+      },
+      1.5
+    );
+  };
+  // {x:583.8576970686372,y:105.97789189016927,z:-702.7101195714305}
+
+  // this.demo = new Demo();
+
+  // 加载结束
   runScene.on("lazyLoadedTexture", () => {
+    // 初始化解析json
+    // 初始化解析数据添加模型
+    // this.addModel.init();
+    // this.resolveJson.addModel();
+    // this.demo.init();
+    // this.tower.init();
+    // this.shop.init();
+    // this.shop.shopPersonEvent();
 
+    controls.maxPolarAngle = Math.PI / 2 - 0.2;
+    controls.screenSpacePanning = false;
+
+    onDone();
   });
 
-  // 加载后回调
-  runScene.on('loaded', (a) => {
+  runScene.on("complete", () => {
+    this.entryAnima();
 
-  })
-
-  // 最后一帧加载回调
-  runScene.on("complete", async () => {
-
-    // 每帧渲染
-    // t.runScene.cb.render.add("每帧渲染注册名称", () => { });
-
-    /**
-     * 注册事件 
-     * 均必须***需要等待场景加载到最后生命周期执行 
-     * (防止报错)***
-     */
+    // 解析数据
+    // this.resolveJson = new ResolveJson();
     this.events = new Events();
+    this.methods = new Methods();
+    this.Camera = new Camera()
+    this.Camera.init()
 
-    // 入场动画
-    this.events.closeAnimaAtStart.enterAnima = Utils.anima({}, {}, (data) => { })
   });
-
-  // 模型特殊处理 加载初始不显示
-  runScene.on('modelLoaded', (models) => {
-    console.log(models, 'models');
-  })
 
   // 销毁
   this.dispose = () => runScene.dispose();
 }
-
+// runScene.modelEx.clone('model')
+// t.runScene.modelEx.clone()
 // 基本事件
 class Events {
   constructor() {
@@ -80,37 +95,15 @@ class Events {
       this.mouseDown
     );
     t.runScene.cb.events.pointer.up.add("pointerUp", this.mouseUp);
+    t.runScene.cb.events.mouse.up.add("mouseUp", this.mouseUp);
     t.runScene.cb.events.mouse.move.add("mouseMove", () => { });
-  }
-
-  showAnima(info) {
-    const { model, isShow, time, cb, opacity } = info;
-    const models = [];
-    model.traverse((m) => {
-      if (m.type === "Group") return;
-      if (m.type === "Object3D") return;
-      m.material.transparent = true;
-      isShow ? (m.material.opacity = 0) : null;
-      models.push(m);
+    t.runScene.cb.render.add("test", () => {
+      t.runScene.assetsEx.controls.update();
     });
-    if (isShow) model.visible = isShow;
-    Utils.anima(
-      { opc: isShow ? 0 : opacity || 1 },
-      { opc: isShow ? opacity || 1 : 0 },
-      time,
-      (data) => {
-        models.map((m) => (m.material.opacity = data.opc));
-      },
-      () => {
-        if (!isShow) model.visible = isShow;
-        cb && cb();
-      }
-    );
   }
-
   downPosition = { x: 0, y: 0 };
 
-  closeAnimaAtStart = { enterAnima: '' };
+  closeAnimaAtStart = {};
 
   mouseDown = (event) => {
     this.downPosition = {
@@ -131,10 +124,45 @@ class Events {
   triggerClick = (e) => {
     const model = t.runScene.modelEx.select;
     if (!model) return;
-    bus.emit('logClickModel', model);
+    console.log("点击到的模型:", model, model.name);
+    console.log(
+      `cx:${camera.position.x},cy:${camera.position.y},cz:${camera.position.z},tx:${controls.target.x},ty:${controls.target.y},tz:${controls.target.z}`,
+      "位置"
+    );
+
+    if (model.name === "摄像头1") {
+      t.Camera.CameraModeShow()
+      t.events.cameraFoucs(
+        {
+          cx:-215.0925207277506,cy:1344.3532850360634,cz:1135.2703397916925,tx:77.812919,ty:296.623976,tz:-250.483781
+        },
+        2
+      );
+    } else {
+      t.Camera.CameraModeHidden()
+    }
+
+    if (model.name === "摄像头2") {
+      t.Camera.CameraModeOneShow()
+      t.events.cameraFoucs(
+        {
+          cx:-242.2893286894415,cy:1284.0330977365038,cz:1324.553015642677,tx:387.313085,ty:296.623976,tz:-237.28282
+        },
+        2
+      );
+    } else {
+      t.Camera.CameraModeOneHidden()
+    }
+
+
   };
 
-  controlStart = () => { };
+  controlStart = () => {
+    // 清除动画
+    Object.values(this.closeAnimaAtStart).map((i) => {
+      i && i.kill();
+    });
+  };
 
   closeAnmia() {
     Object.values(this.closeAnimaAtStart).map(
@@ -143,41 +171,127 @@ class Events {
         item && item.kill()
     );
   }
-
   dispose() {
     dom.removeEventListener("pointerdown", this.mouseDown);
     dom.removeEventListener("pointerup", this.mouseUp);
     controls.removeEventListener("start", this.controlStart);
   }
+
+
+  // 视角转换
+  cameraFoucs(position, time = 1) {
+    t.events.closeAnimaAtStart.anima = Utils.anima(
+      {
+        cx: t.runScene.assetsEx.camera.position.x,
+        cy: t.runScene.assetsEx.camera.position.y,
+        cz: t.runScene.assetsEx.camera.position.z,
+        tx: t.runScene.assetsEx.controls.target.x,
+        ty: t.runScene.assetsEx.controls.target.y,
+        tz: t.runScene.assetsEx.controls.target.z,
+      },
+      {
+        ...position,
+      },
+      time,
+      (data) => {
+        t.runScene.assetsEx.camera.position.set(data.cx, data.cy, data.cz);
+        t.runScene.assetsEx.controls.target.set(data.tx, data.ty, data.tz);
+        t.runScene.assetsEx.camera.updateProjectionMatrix();
+        t.runScene.assetsEx.controls.update();
+      }
+    );
+  }
+
+}
+
+
+class Camera {
+  // 摄像头1模型
+  Cameramodel = null;
+  // 摄像头2dom模型
+  CameramodelOne = null;
+  // 摄像头1 2D元素
+  Camersprite = null;
+  // 摄像头2 2D元素
+  CamerspriteOne = null
+  init() {
+    this.createDom()
+  }
+  createDom() {
+    Utils.getMacro(() => {
+
+      let createDom = document.querySelectorAll(".camera")
+      let timer = setTimeout(() => {
+        createDom[0].classList.add('show');
+        createDom[1].classList.add('show');
+        clearTimeout(timer)
+      }, 100);
+
+
+      this.Cameramodel = t.runScene.modelEx.getModel("摄像头1")
+      // this.Camersprite = Utils.domTo3DSprite(createDom[0])
+      this.Camersprite = Utils.domTo2Dui(createDom[0])
+      this.Camersprite.visible = false;
+
+      this.Camersprite.position.x += 130
+      this.Camersprite.position.y += 500
+      t.runScene.modelEx.add(this.Camersprite)
+
+      this.CameramodelOne = t.runScene.modelEx.getModel("摄像头2")
+      // this.CamerspriteOne = Utils.domTo3DSprite(createDom[1])
+      this.CamerspriteOne = Utils.domTo2Dui(createDom[1])
+      this.CamerspriteOne.visible = false;
+
+      this.CamerspriteOne.position.x += 1350
+      this.CamerspriteOne.position.y += 400
+      t.runScene.modelEx.add(this.CamerspriteOne)
+    })
+  }
+
+  CameraModeShow() {
+    bus.emit('showCamera')
+    this.Camersprite.visible = true;
+  }
+  CameraModeHidden() {
+    this.Camersprite.visible = false;
+  }
+
+  CameraModeOneShow() {
+    bus.emit('showCameraOne')
+    this.CamerspriteOne.visible = true;
+  }
+  CameraModeOneHidden() {
+    this.CamerspriteOne.visible = false;
+  }
+
+}
+//方法
+class Methods {
+  //获取模型
+  getModel(name) {
+    return t.runScene.modelEx.getModel(name);
+  }
+  //相机动画
+  camAnima(view, time, callback) {
+    t.runScene.modelEx.camAnima(
+      t.runScene.modelEx.getCamLocal(),
+      view,
+      time,
+      () => {
+        callback && callback();
+      }
+    );
+  }
+  //场景自转
+  autoRotation(isRotate, speed, lookat) {
+    t.runScene.assetsEx.controls.autoRotate = isRotate;
+    t.runScene.assetsEx.controls.autoRotateSpeed = speed;
+    lookat && t.runScene.camera.lookAt(lookat.x, lookat.y, lookat.z);
+  }
+  //开启辉光
+  light(model, isOpen) {
+    t.runScene.modelEx.setGlow(model, isOpen);
+  }
 }
 
 export default Change;
-
-
-/* 常用run-scene-v--API 详细的github自取
-   版本更新飞书自查
-*/
-
-/**
- * 获取模型
- * t.runScene.modelEx.getModel('');
- * 
- * 基本的场景配置
- * controls.maxPolarAngle = Math.PI / 2 - 0.2;
- * controls.screenSpacePanning = false;
- * 
- * 转dom
- * Utils.domTo3DSprite(dom);
- * Utils.domTo2Dui(dom);
- * Utils.domTo3Dui(dom);
- * 
- * 相机当前位置以及视角
- * Utils.getCamAnimaData()
- *  
- * 宏任务(自带清计时器)
- * Utils.getMacro(()={},time)
- * 
- * 补帧动画
- * Utils.anima({},{},time,(data)=>{},()=>{})
- * 
- * */
--- src/components/three-scene/index.vue
+++ src/components/three-scene/index.vue
@@ -1,30 +1,28 @@
 <template>
   <div class="three-scene" ref="three-scene" onselectstart="return false;">
     <div
+      class="btn"
       @pointerdown="
         (e) => {
           // e.preventDefault();
           e.stopPropagation();
         }
       "
-      class="btn"
-    >
-      <button @click="showCamera1 = !showCamera1">摄像头1</button>
-      <button @click="showCamera2 = !showCamera2">摄像头2</button>
-    </div>
-
-    <div class="camera1 camera">
+    ></div>
+    <div class="camera camera1">
+      <div class="delete" @pointerdown="noShow">X</div>
       <monitor
         v-show="showCamera1"
-        url="1"
+        url="2"
         ws="ws://192.168.3.8:8888/rtsp/"
         type="flv"
       ></monitor>
     </div>
-    <div class="camera2 camera">
+    <div class="camera camera2">
+      <div class="delete" @pointerdown="oneShow">X</div>
       <monitor
         v-show="showCamera2"
-        url="2"
+        url="1"
         ws="ws://192.168.3.8:8888/rtsp/"
         type="flv"
       ></monitor>
@@ -37,6 +35,7 @@ import Change from "./Change";
 import { RunScene, Utils } from "run-scene-v2";
 import bus from "./Bus";
 import monitor from "./camera.vue";
+// 场景的传值Bus
 export default {
   components: {
     monitor,
@@ -45,6 +44,7 @@ export default {
     return {
       change: null,
       runScene: null,
+      isShow: false,
       Bus: bus,
       showCamera1: false,
       showCamera2: false,
@@ -54,122 +54,125 @@ export default {
     // 加载场景
     this.loadScene();
     // 打印点击的模型接口
-    bus.on("logClickModel", this.logClickModel);
+    // bus.$on("logClickModel", this.logClickModel);
+
+    bus.on("showCamera", () => {
+      // this.isShow = !this.isShow;
+      this.showCamera1 = true;
+    });
+    bus.on("showCameraOne", () => {
+      // this.isShow = !this.isShow;
+      this.showCamera2 = true;
+      console.log("11");
+    });
   },
   methods: {
     // 加载场景
     loadScene() {
       this.runScene = new RunScene({
+        // path: "https://test2-1303915342.cos.ap-shanghai.myqcloud.com/DreamShowroom/scene.glb",
         path: "./assets/scene.glb",
         rootDom: this.$refs["three-scene"],
         options: {
-          // render2: true,
+          // notTexture: true,
+          render2: true,
           // render3: true,
-          texture: {
-            // load: false,
-            // lazyload: {
-            //   open: true,
-            //   IntervalTime: 16.6,
-            // },
-          },
-          /**
-            msg?: {
-            是否显示打印，默认显示
-              show: boolean = true
-             显示打印的等级 默认显示基础打印
-              level: "base" | 'detail' =  base
-              }
-             是否渲染
-              run?: boolean = true
-             decode 的路径
-              decodePath?: string = ./draco/
-             是否显示fps 默认关
-              showFps?: boolean = false
-             是否延迟加载 默认不延迟
-              loadInterval?: number = 0
-             模式 默认运行模式
-              mode?: 'editor' | 'debug' | 'running' = 'running
-             texture?:{
-             是否加载贴图
-               load?:boolean = true
-               lazyload?:{
-             是否懒加载贴图 默认是
-                 open?:boolean = false,
-             懒加载的时间区间 默认为16.0ms
-                 IntervalTime?:number = 16.6
-              },
-             贴图质量 可大幅度降低显存占用 0-1 之间
-              quality?:number = 1
-             }
-             是否加载实例后的模型 节省性能 默认关闭
-              instanceClone?: boolean =false
-             2drenderer
-              render2?: boolean = false
-             3drenderer
-              render3?: boolean, = false
-               */
         },
-      }).on("complete", () => {});
-      this.change = new Change(this.runScene);
+        // controlsUpdate: false,
+      });
+      this.change = new Change(this.runScene, this.onDone);
     },
-
     onDone() {
-      console.log("场景加载完毕~");
+      console.log("场景加载结束~");
     },
-
     // 打印点击到的模型
     logClickModel(model) {
       console.log("点击的模型为:", model.name);
     },
-  },
-  // 场景自带销毁
-  destroyed() {
-    this.runScene && this.runScene.dispose();
+    // 事件一
+    events1() {
+      console.log(1);
+    },
+    noShow() {
+      this.change.Camera.CameraModeHidden();
+      console.log("11");
+    },
+    oneShow() {
+      this.change.Camera.CameraModeOneHidden();
+    },
   },
 };
 </script>
 
 <style lang="less" scoped>
-// 场景
 .three-scene {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   position: relative;
-
   .btn {
+    z-index: 10;
     position: absolute;
-    z-index: 2;
+    .floor {
+      button {
+        color: black;
+      }
+    }
   }
-
   .camera {
-    position: absolute;
-    bottom: 0;
-    z-index: 2;
-    width: 30%;
-    height: 30%;
+    // position: absolute;
+    // bottom: -20%;
+    // z-index: 2;
+    // width: 25%;
+    // height: 25%;
+    // background-color: rgba(0, 183, 255, 0.215);
+    // position: absolute;
+    // bottom: 10%;
+    width: 25%;
+    // height: 252px;
+    opacity: 0;
   }
 
   .camera1 {
     .camera;
-    left: 0;
+    // left: 19%;
   }
 
   .camera2 {
     .camera;
-    right: 0;
+    // right: -54%;
+  }
+  .delete {
+    position: absolute;
+    top: 1%;
+    left: 94%;
+    width: 5%;
+    height: 10%;
+    // background-color: rgb(0, 55, 255);
+    z-index: 10;
+    text-align: center;
+    line-height: 30px;
   }
 
   .oth {
     position: absolute;
     z-index: 2;
   }
-
   .show {
     opacity: 1 !important;
   }
-
   .none {
     opacity: 0 !important;
   }
+}
+.shopBorder {
+  z-index: 100;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  // background-color: red;
+  opacity: 0;
+}
+.showOpacity {
+  opacity: 1 !important;
 }
 </style>
